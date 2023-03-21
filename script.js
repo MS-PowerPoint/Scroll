b9380c83f692014efe58d0dc1660c6cffcdc1623
MS-PowerPoint <114092392+MS-PowerPoint@users.noreply.github.com>
2023-03-21 22:42:47 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,23 @@
 const text = document.querySelector('.text');
 const Scroll = document.querySelector('#scroll');
 let pixel = 0;
+let pixel1 = 0;
 let time1;
-let timer = 0;
-
+let scrooll = 0;
+let timer = 1;
+let bool = true;
 
 document.addEventListener('wheel', MouseScroll);
 document.addEventListener(e);
 
-
-
-
 function MouseScroll(e) {
-
-    time1 = setInterval(()=>{timer++},1000)
-    if (e.deltaY != 0) {
-        pixel += Math.abs(e.deltaY);
-        pixel = pixel/timer;
-        console.log(pixel);
-        text.innerHTML = pixel+' scroll';
+    if (bool == true) {
+        time1 = setInterval(() => { timer++; console.log(timer) }, 1000); w
+        bool = false;
     }
 
+    pixel += Math.abs(e.deltaY);
+    scrooll = pixel / timer;
+    text.innerHTML = parseFloat(scrooll).toFixed(2); +' scroll';
+
 }
-
-
-
-
